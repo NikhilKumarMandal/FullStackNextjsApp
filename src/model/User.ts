8021067fff9ce5f661cc81a6320f9a8bcf46c24c
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-04-13 21:21:17 +0530

    "Add user verification and message features to sign-up route; update User model to include 'messages' array"
    This commit adds user verification functionality to the sign-up route by checking for existing users with the same username or email. If

--- src/model/User.ts
+++ src/model/User.ts
@@ -27,7 +27,7 @@ export interface User extends Document{
     verifyCodeExpiry: Date;
     isVerified: boolean;
     isAcceptingMessage: boolean;
-    message: Message[]
+    messages: Message[]
 }
 
 
@@ -64,7 +64,7 @@ const UserSchema: Schema<User> = new Schema({
         type: Boolean,
         default: false
     },
-    message: [MessageSchema]
+    messages: [MessageSchema]
 })
 
 const UserModel =(mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>('User',UserSchema)
